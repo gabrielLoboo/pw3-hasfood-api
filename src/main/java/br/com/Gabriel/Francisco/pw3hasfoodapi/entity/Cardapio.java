2b418dc9acf22a820c8309b37f022de1f03666e3
Gabriel Francisco Lobo <gabriel@gmail.com>
2022-03-28 08:14:45 -0300

    Criacao da entidade Cardapio

---
+++ src/main/java/br/com/Gabriel/Francisco/pw3hasfoodapi/entity/Cardapio.java
@@ -0,0 +1,19 @@
+package br.com.Gabriel.Francisco.pw3hasfoodapi.entity;
+
+import javax.persistence.Entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@Column
+public class Cardapio {
+	
+	//CREATE TABLE TBL_CARDAPIO (
+			//ID_CARDAPIO BIGINT PRIMARY KEY IDENTITY,
+			//ID_RESTAURANTE BIGINT NOT NULL
+			//);	
+	
+}
---
+++ src/main/java/br/com/Gabriel/Francisco/pw3hasfoodapi/entity/Column.java
@@ -0,0 +1,5 @@
+package br.com.Gabriel.Francisco.pw3hasfoodapi.entity;
+
+public @interface Column {
+
+}
